ca008b497c4930862114a1e9dcd97d83514c737d
zhaoxy8 <zhaoxingyuan_pm@163.com>
2020-05-09 09:13:12 +0000

    init.MapTo

--- aispace.com/logagent/conf/conf.go
+++ aispace.com/logagent/conf/conf.go
@@ -1,30 +1,15 @@
 package conf
 
-import (
-	"fmt"
-	"os"
-	"strings"
-
-	"gopkg.in/ini.v1"
-)
-
 //Conf ...
 type Conf struct {
-	Hosts []string
-	Topic string
-	Path  string
+	KafkaConf   `ini:"kafka"`
+	TaillogConf `ini:"taillog"`
 }
 
-//NewConf ...
-func NewConf(filename string) *Conf {
-	cfg, err := ini.Load(filename)
-	if err != nil {
-		fmt.Printf("Fail to read file: %v", err)
-		os.Exit(1)
-	}
-	return &Conf{
-		Hosts: strings.Split(cfg.Section("kafka").Key("hosts").String(), ","),
-		Topic: cfg.Section("kafka").Key("topic").String(),
-		Path:  cfg.Section("taillog").Key("path").String(),
-	}
+type KafkaConf struct {
+	Address []string `ini:"hosts"`
+	Topic   string   `ini:"topic"`
+}
+type TaillogConf struct {
+	Path string `ini:"path"`
 }
--- aispace.com/logagent/main.go
+++ aispace.com/logagent/main.go
@@ -9,22 +9,30 @@ import (
 	"Golong/aispace.com/logagent/taillog"
 )
 
-func run(conf *conf.Conf) {
+var (
+	cfg = new(conf.Conf)
+)
+
+func run() {
 	for true {
 		select {
 		case line := <-taillog.ReadChan():
 			fmt.Println("line:", line.Text)
-			kafka.SendMsg(conf.Topic, line.Text)
+			kafka.SendMsg(cfg.KafkaConf.Topic, line.Text)
 		default:
 			time.Sleep(100 * time.Millisecond)
 		}
 	}
 }
+
 func main() {
-	conf := conf.NewConf("conf.ini")
-	taillog.Init(conf.Path)
+	err = init.MapTo(cfg, "./conf.ini")
+	if err != nil {
+		fmt.Printf("load conf.init failed", err)
+	}
+	taillog.Init(cfg.TaillogConf.Path)
 	fmt.Println("taillog init success")
-	kafka.Init(conf.Hosts)
+	kafka.Init(cfg.KafkaConf.Address)
 	fmt.Println("kafka init success")
-	run(conf)
+	run()
 }
