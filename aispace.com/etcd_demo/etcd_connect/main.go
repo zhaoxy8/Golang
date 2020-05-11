39a91a10d9e9467df559e97d50f3bcb16e0df944
xyzhao8 <zhaoxingyuan_pm@163.com>
2020-05-12 00:25:44 +0800

    添加etcd测试代码

--- aispace.com/etcd_demo/etcd_connect/main.go
+++ aispace.com/etcd_demo/etcd_connect/main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"context"
 	"fmt"
@@ -6,9 +7,35 @@ import (
 
 	"go.etcd.io/etcd/clientv3"
 )
-func main(){
-	cli,err := clientv3.New(clientv3.Config{
-		Endpoints:[]string{"11.81.1.164:2379"},
-		DialTimeout: 5*time.Second,
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"161.189.201.174:2379"},
+		DialTimeout: 5 * time.Second,
 	})
+	if err != nil {
+		fmt.Printf("connect to etcd failed,err：%v\n", err)
+		return
+	}
+	fmt.Println("connect to etcd success")
+	defer cli.Close()
+	//put
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	_, err = cli.Put(ctx, "/logpath", "wanglei")
+	cancel()
+	if err != nil {
+		fmt.Printf("put to etcd failed, err:%v\n", err)
+		return
+	}
+	//get
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := cli.Get(ctx, "/logpath")
+	if err != nil {
+		fmt.Printf("get from etcd failed, err:%v\n", err)
+		return
+	}
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s:%s\n", ev.Key, ev.Value)
+	}
+
 }
